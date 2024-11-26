e7da02e2c967db9cec15c7423e69b7ae8171312e
nagisa <136070735+mitsui6969@users.noreply.github.com>
2024-11-27 06:42:34 +0900

    画像選択可能

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:clipit_front/components/footer.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: '.env');
   runApp(const ClipItApp());
 }
 
