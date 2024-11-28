2ee389181988016ccd0ab11fab942b3d6608a2fd
nagisa <136070735+mitsui6969@users.noreply.github.com>
2024-11-28 16:15:23 +0900

    firebaseに画像アップロード可能

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:clipit_front/components/footer.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
