364a7a5e1dc8fe8d33e6e64e59c314c26f7ed103
nagisa <136070735+mitsui6969@users.noreply.github.com>
2024-12-01 03:49:14 +0900

    角丸と色変更

--- lib/components/topic_container.dart
+++ lib/components/topic_container.dart
@@ -13,15 +13,15 @@ class TopicContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(
-        vertical: 12,
-        horizontal: 16,
+        vertical: 15,
+        horizontal: 30,
       ),
       child: Container(
-        height: 220,
+        height: 230,
         decoration: const BoxDecoration(
-          color: Color(0xFFf0f0f0),
+          color: Color(0xFFFAB800),
           borderRadius: BorderRadius.all(
-            Radius.circular(5),
+            Radius.circular(15),
           ),
         ),
         child: Column(
@@ -36,7 +36,7 @@ class TopicContainer extends StatelessWidget {
                 horizontal: 15
               ),
               decoration: const BoxDecoration(
-                color: Color(0xFFFAB800),
+                // color: Color(0xFFFAB800),
                 borderRadius: BorderRadius.vertical(
                   top: Radius.circular(5),
                 ),
@@ -47,12 +47,19 @@ class TopicContainer extends StatelessWidget {
             // 画像部分
             Container(
               height: 170,
+              padding: const EdgeInsets.symmetric(
+                // vertical: 15,
+                horizontal: 10,
+              ),
               decoration: const BoxDecoration(
                 borderRadius: BorderRadius.all(
-                  Radius.circular(5)
+                  Radius.circular(10)
                 ),
               ),
               child: ClipRRect(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(8),
+                ),
                 child: Image.network(
                   topic.image_url,
                   fit: BoxFit.cover,
